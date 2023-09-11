520abf5142c803feb2d1a68c72d40fc93d925505
Nathan Tirolf <nathan.tirolf@epitech.eu>
2023-09-11 21:38:00 +0200

    [CLEAN] : old values

--- include/gameSlot.h
+++ include/gameSlot.h
@@ -22,13 +22,7 @@
 
     typedef enum GAME {
         NO_GAME = -1,
-        GAME_SNAKE,
-        GAME_SNAKE1,
-        GAME_SNAKE2,
-        GAME_SNAKE3,
-        GAME_SNAKE4,
-        GAME_SNAKE5,
-        GAME_SNAKE6,
+        SNAKE,
         GAME_MAX,
     } GAME_E;
 
